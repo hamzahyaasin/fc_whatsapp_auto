8cac395d9dfe9f8d2433c1b577df30ec071777e2
Hamza <hamzasmart07@gmail.com>
2024-04-28 19:40:57 +0500

    Hamza pushing the properly working code now

---
+++ t.py
@@ -0,0 +1,8 @@
+print('''Dear Student,
+
+Hope you are doing well.
+
+We hope your learning is going well, if you would like to schedule a session, please let us know. If you would like assistance in any other area, please feel free to get in touch with us. Happy Learning.
+
+Best regards,
+Future Connect Training & Recruitment Ltd.''')
